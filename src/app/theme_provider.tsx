9fbcce85e8ee1452bdf804f5497136a0eb009a9e
exsjabe <jakob.bergstrom@explizit.se>
2022-12-28 02:21:20 +0100

    Removed progress bar

--- src/app/theme_provider.tsx
+++ src/app/theme_provider.tsx
@@ -1,12 +1,10 @@
 'use client';
 import type {ChildrenProps} from 'props';
 import {SkeletonTheme} from 'react-loading-skeleton';
-import NextNProgress from 'nextjs-progressbar';
 
 export const ThemeProvider = ({children}: ChildrenProps) => {
   return (
     <>
-      <NextNProgress color="#F28C18" />
       <SkeletonTheme baseColor="#212121">{children}</SkeletonTheme>
     </>
   );
